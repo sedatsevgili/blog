5aa6d39028a9916898b758b6d30b62285e7f1976
Sedat <sedatsevgili@gmail.com>
2024-03-03 15:32:42 +0300

    try fixing escape issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -78,7 +78,7 @@ export default class MyDocument extends Document {
                         case 0:
                             segmentConsent = true;
                             defaultConsent = false;
-                            mainSegmentScript = "!function(){var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");n.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey="".concat(segmentKey, "";;analytics.SNIPPET_VERSION="5.2.0";\n          analytics.load("").concat(segmentKey, "");\n          }}();");
+                            mainSegmentScript = "!function(){var i=\"analytics\",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error(\"Segment snippet included twice.\");else{analytics.invoked=!0;analytics.methods=[\"trackSubmit\",\"trackClick\",\"trackLink\",\"trackForm\",\"pageview\",\"identify\",\"reset\",\"group\",\"track\",\"ready\",\"alias\",\"debug\",\"page\",\"screen\",\"once\",\"off\",\"on\",\"addSourceMiddleware\",\"addIntegrationMiddleware\",\"setAnonymousId\",\"addDestinationMiddleware\",\"register\"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if([\"track\",\"screen\",\"alias\",\"group\",\"page\",\"identify\"].indexOf(e)>-1){var c=document.querySelector(\"link[rel='canonical']\");n.push({__t:\"bpc\",c:c&&c.getAttribute(\"href\")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement(\"script\");t.type=\"text/javascript\";t.async=!0;t.setAttribute(\"data-global-segment-analytics-key\",i);t.src=\"https://cdn.segment.com/analytics.js/v1/\" + key + \"/analytics.min.js\";var r=document.getElementsByTagName(\"script\")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey=\"".concat(segmentKey, "\";;analytics.SNIPPET_VERSION=\"5.2.0\";\n          analytics.load(\"").concat(segmentKey, "\");\n          }}();");
                             return [4 /*yield*/, fetchDestinations()];
                         case 1:
                             destinations = _a.sent();
@@ -151,7 +151,7 @@ export default class MyDocument extends Document {
                 return null;
             };
             convertToSafeJSON = function (cookie) {
-                return cookie.replace(/'/g, '"').replace(/(w+):/g, '"$1":');
+                return cookie.replace(/'/g, '"').replace(/(\w+):/g, '"$1":');
             };
             fetchDestinations = function () { return __awaiter(void 0, void 0, void 0, function () {
                 var response, data;
@@ -174,7 +174,7 @@ export default class MyDocument extends Document {
             return [2 /*return*/];
         });
     }); };
-    segmentCookieBotAnalyticsWrapper();
+    segmentCookieBotAnalyticsWrapper();    
     `
   }
 
